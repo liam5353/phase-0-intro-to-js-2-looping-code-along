31c2cf971f769830a28c116e38d62ae0d823f32e
“Liam <okeefe.liam@yahoo.com>
2022-09-08 19:32:35 -0500

    commit tree 977009ba38ede6aed98f86370fe6af9e9262751e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards  (names, event){
+const thankYou = []
+for (let i = 0; i < names.length; i++) {
+console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+thankYou.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+}
+console.log (thankYou)
+
+return thankYou
+}
+
+function countDown (numbers){
+let countdown = 0;
+while (countdown < 11) {
+  console.log(countdown++);
+}
+}
